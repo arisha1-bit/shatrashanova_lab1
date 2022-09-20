4c47c6427bdd19dc0963c766e07aa3203e65ed61
arisha1-bit <arishastark5@gmail.com>
2022-09-20 21:08:18 +0300

    добавила вывод в файл

--- shatrashanova_lab1/shatrashanova_lab1/shatrashanova_lab1.cpp
+++ shatrashanova_lab1/shatrashanova_lab1/shatrashanova_lab1.cpp
@@ -1,7 +1,5 @@
-﻿// shatrashanova1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
 using namespace std;
 struct Pipe
 {
@@ -56,7 +54,6 @@ int check_workshop_cin(int x, int y)
     }
     return x;
 }
-
 int main()
 {
     int option = -1;
@@ -66,72 +63,79 @@ int main()
         cout << "\nChoose option:\n 1.Add pipe 2.Add CS 3.View all objects " <<
             "4.Edit pipe 5.Edit CS 6.Save 7.Load 0.Exit\n";
         cin >> option;
-        if (option == 1) {
+        switch (option) {
+            case 1: {
             cout << "\nInput lenght ";
             cin >> p.lenght;
-            p.lenght= check_cin(p.lenght);
+            p.lenght = check_cin(p.lenght);
             cout << "\nInput diameter ";
             cin >> p.diameter;
-            p.diameter=check_cin(p.diameter);
+            p.diameter = check_cin(p.diameter);
             cout << "\nChoose status of pipe (1 if repairing, 2 if works) ";
             cin >> p.status;
-            p.status=check_status_cin(p.status);
+            p.status = check_status_cin(p.status);
             cout << status_check(p.status) << endl;
+            break;
         }
-        if (option == 2) {
+            case 2: {
             cout << "\nInput name ";
             cin >> cs.name;
             cout << "\nNumber of workshops ";
             cin >> cs.workshop;
-            cs.workshop=check_cin(cs.workshop);
+            cs.workshop = check_cin(cs.workshop);
             cout << "\nNumber of working workshops ";
             cin >> cs.working_workshop;
-            cs.working_workshop=check_workshop_cin(cs.working_workshop, cs.workshop);
+            cs.working_workshop = check_workshop_cin(cs.working_workshop, cs.workshop);
             cs.effectiveness = float(cs.workshop) / float(cs.working_workshop);
             cout << "\nEffectiveness: " << cs.effectiveness << endl;
+            break;
         }
-        if (option == 3) {
-            cout << "\nPipe info:\nLenght: " << p.lenght << "\nDiameter: " << p.diameter << "\nStatus: " << status_check(p.status) << endl;;
-            cout << "\n\nCS info:\nName: " << cs.name << "\nNumber of workshops: " << cs.workshop << "\nNumber of working workshops: " << cs.working_workshop << "\nEffectiveness: " << cs.effectiveness << "\n\n\n";
+            case 3: {
+            cout << "\nPipe info:\nLenght: " << p.lenght << "\nDiameter: " << p.diameter 
+                << "\nStatus: " << status_check(p.status) << endl;;
+            cout << "\n\nCS info:\nName: " << cs.name << "\nNumber of workshops: " << cs.workshop
+                << "\nNumber of working workshops: " << cs.working_workshop << "\nEffectiveness: "
+                << cs.effectiveness << "\n\n\n";
+            break;
         }
-        if (option == 4) {
+            case 4: {
             if (p.status == -1)
                 cout << "\nThere is no pipe to edit" << endl;
             else {
                 cout << "Input new status of pipe (1 if pipe is repairing, 2 if pipe works" << endl;
                 cin >> p.status;
-                p.status=check_status_cin(p.status);
+                p.status = check_status_cin(p.status);
                 cout << status_check(p.status) << endl;
             }
-
-
+            break;
         }
-        if (option == 5) {
-            if (cs.working_workshop==-1)
+            case 5: {
+            if (cs.working_workshop == -1)
                 cout << "\nThere is no CS to edit" << endl;
             else {
                 cout << "Input new number of working workshops: " << endl;
                 cin >> cs.working_workshop;
-                cs.working_workshop=check_workshop_cin(cs.working_workshop, cs.workshop);
+                cs.working_workshop = check_workshop_cin(cs.working_workshop, cs.workshop);
                 cs.effectiveness = float(cs.workshop) / float(cs.working_workshop);
                 cout << "\nEffectiveness: " << cs.effectiveness << endl;
             }
+            break;
         }
-        if (option == 0) {
+            case 6: {
+                ofstream file;
+                file.open("saved_information.txt");
+                file << "\nPipe info:\nLenght: " << p.lenght << "\nDiameter: " << p.diameter 
+                    << "\nStatus: " << status_check(p.status) << "\n\nCS info:\nName: " << cs.name
+                    << "\nNumber of workshops: " << cs.workshop << "\nNumber of working workshops: " 
+                    << cs.working_workshop << "\nEffectiveness: " << cs.effectiveness << "\n\n\n";
+                file.close();
+                break;
+            }
+            case 7: break;
+            case 0: {
             return 0;
+        }
         }
     }
     return 0;
 }
-
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
